689e65770a4e31a7bd83e193463957df56d83b8e
HONGYI-SD <ubuntu@ip-172-31-35-166.ap-east-1.compute.internal>
2024-06-06 06:16:03 +0000

    ops: format codes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-ord-tx-example
 go 1.20
 
 require (
+	github.com/MultiAdaptive/kzg-sdk v1.1.5
 	github.com/btcsuite/btcd v0.24.0
 	github.com/btcsuite/btcd/btcec/v2 v2.3.3
 	github.com/btcsuite/btcd/btcutil v1.1.5
@@ -12,7 +13,6 @@ require (
 )
 
 require (
-	github.com/MultiAdaptive/kzg-sdk v1.1.5 // indirect
 	github.com/MultiAdaptive/sig-sdk v0.1.1 // indirect
 	github.com/aead/siphash v1.0.1 // indirect
 	github.com/bits-and-blooms/bitset v1.10.0 // indirect
@@ -24,12 +24,10 @@ require (
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/ethereum/go-ethereum v1.13.1 // indirect
-	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
@@ -37,4 +35,4 @@ require (
 
 replace github.com/vincentdebug/go-ord-tx => ../go-ord-tx
 
-replace github.com/MultiAdaptive/sigsdk => ../sig-sdk
+replace github.com/MultiAdaptive/sig-sdk => ../sig-sdk
--- inscribewithprivatenoderpc/main.go
+++ inscribewithprivatenoderpc/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/hex"
 	"log"
 
 	kzg_sdk "github.com/MultiAdaptive/kzg-sdk"
-	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
@@ -31,14 +31,8 @@ func main() {
 	defer client.Shutdown()
 
 	commitTxOutPointList := make([]*wire.OutPoint, 0)
-	// you can get from `client.ListUnspent()`
-	utxoAddress := "bcrt1qkdv0tchxy33ctx4h33squ0ym39say6dwxwvuft"
-	address, err := btcutil.DecodeAddress(utxoAddress, netParams)
-	if err != nil {
-		log.Fatalf("decode address err %v", err)
-	}
-	unspentList, err := client.ListUnspentMinMaxAddresses(1, 9999999, []btcutil.Address{address})
 
+	unspentList, err := client.ListUnspent()
 	if err != nil {
 		log.Fatalf("list err err %v", err)
 	}
@@ -59,11 +53,16 @@ func main() {
 		log.Fatalf("kzg sdk GenerateDataCommit failed")
 	}
 	dataCM := cm.Bytes()
+	log.Println("dataCM:", hex.EncodeToString(dataCM[:]))
+	changeaddr, err := client.GetNewAddress("default")
+	if err != nil {
+		log.Fatalf("GetRawChangeAddress failed with error:%v", err)
+	}
 	dataList := make([]ord.InscriptionData, 0)
 	dataList = append(dataList, ord.InscriptionData{
 		ContentType: "text/plain;charset=utf-8",
 		Body:        dataCM[:],
-		Destination: "tb1p3m6qfu0mzkxsmaue0hwekrxm2nxfjjrmv4dvy94gxs8c3s7zns6qcgf8ef",
+		Destination: changeaddr.String(),
 	})
 
 	request := ord.InscriptionRequest{
@@ -94,26 +93,4 @@ func main() {
 		log.Println("inscription, " + inscriptions[i])
 	}
 	log.Println("fees: ", fees)
-	// signet server
-	// http://signet.ordinals.com/
-	// https://signet.ordapi.xyz/
-	// https://signet.earlyordies.com/
-	// https://explorer-signet.openordex.org/
-
-	//commitTxHash, 1ead71ecc9b17d449ec86ef217c5fd9476b8c7c27220834f9eadac32068d6194
-	//revealTxHash, aafc791814d098cd5c7a9750812c532cbd415f0426b92c9482497cd53041ab59
-	//revealTxHash, 1fd91dd783c041872583826840947d4c28f10770a059e69da9cf7c48ce77a016
-	//revealTxHash, 7a003b0534c8a840d61ab7555daa8701da9f619a80aba8858d4f101cd7d62ee4
-	// http://signet.ordinals.com/inscription/aafc791814d098cd5c7a9750812c532cbd415f0426b92c9482497cd53041ab59i0
-	// http://signet.ordinals.com/inscription/1fd91dd783c041872583826840947d4c28f10770a059e69da9cf7c48ce77a016i0
-	// http://signet.ordinals.com/inscription/7a003b0534c8a840d61ab7555daa8701da9f619a80aba8858d4f101cd7d62ee4i0
-
-	//commitTxHash, b752d80e97196582fd02303f76b4b886c222070323fb7ccd425f6c89f5445f6c
-	//revealTxHash, dceab59e310b94612dd2b746c188e1a4f5bb0f3d77c6b10d220c37951631f36a
-	// http://signet.ordinals.com/inscription/dceab59e310b94612dd2b746c188e1a4f5bb0f3d77c6b10d220c37951631f36ai0
-	// Currently, the official Ordinal parser can only parse a single NFT per transaction.
-	// When the official Ordinal parser supports parsing multiple NFTs in the future, https://github.com/casey/ord/blob/0.5.1/src/inscription.rs#L32
-	// we can consider using a single reveal transaction.
-	// http://signet.ordinals.com/inscription/dceab59e310b94612dd2b746c188e1a4f5bb0f3d77c6b10d220c37951631f36ai1
-	// http://signet.ordinals.com/inscription/dceab59e310b94612dd2b746c188e1a4f5bb0f3d77c6b10d220c37951631f36ai2
 }
