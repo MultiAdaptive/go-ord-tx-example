9da9a296386843d31a0fd485656aeef11264ed22
HONGYI-SD <ubuntu@ip-172-31-35-166.ap-east-1.compute.internal>
2024-06-12 08:28:59 +0000

    ops: upate multiadaptive node rpcurl

--- inscribewithprivatenoderpc/test-script.sh
+++ inscribewithprivatenoderpc/test-script.sh
@@ -3,14 +3,14 @@ export btcRPC="http://13.228.170.151:18443/"
 export BTCHOST="13.228.170.151:18443/wallet/test-wallet-1"
 export BTCUSER="multiadaptiveUser1"
 export BTCPASS="pwd123"
-export MULTIADAPTIVENODERPC="http://13.125.118.52:8545"
-export MULTIADAPTIVENODEPUBKEY="020b0bae055c4e33c8561c080e8dd6c80b9f40f4a7fdf406c8c1da3b68dbc8a9f2"
+export MULTIADAPTIVENODERPC="http://54.86.78.227:8545"
+export MULTIADAPTIVENODEPUBKEY="024063dc56b68904e2f7c0e4ddee10d2da9625d4bdf2fe0002cdf381bf3d13f7cb"
 
 inscriptDACM="./sendBTCDA"
 mineCommand="curl -X POST --user $BTCUSER:$BTCPASS -H 'content-type: text/plain;' $btcRPC --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"generatetoaddress\", \"params\": [1,\"bcrt1qatdxfq0ttd4kxhpxrgc07mpx2p0rpcaua26jht\"] }'"
 
 while true; do
-  eval $mineCommand
   eval $inscriptDACM
+  eval $mineCommand
   sleep 10
 done
